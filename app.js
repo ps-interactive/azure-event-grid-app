5ce0d04ba3d4cf4a17b972e0ffe0a5055e65db41
Tom Bell <tjbell@gmail.com>
2020-10-05 14:55:39 -0600

    refactor: carvedrock store

--- app.js
+++ app.js
@@ -1,41 +1,49 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('/', (req, res) => res.render('index'));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+const randomID = () => '_' + Math.random().toString(36).substr(2, 9);
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.get('/store', (req, res) =>  res.render('store'));
+app.post('/store', (req, res) => {
+  const OrderID = randomID();
+  const UserID = randomID();
+  const ProductID = randomID();
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  /**************
+   Send Messages 
+  **************/
+  async function main() {
+    const endpoint = "";
+    const accessKey = "";
+    const client = new EventGridPublisherClient(endpoint, new AzureKeyCredential(accessKey));
+    await client.sendEvents([
+      {
+        eventType: "CR.Orders.NewOrder",
+        subject: "orders/new/online",
+        dataVersion: "1.0",
+        data: {
+          message: `OrderID: ${OrderID}, UserID: ${UserID}, SKU: ${ProductID}`,
+        }
+      }
+    ]);
+  }
+
+  main().catch((err) => {
+    res.render('index', { title: `An error was encountered: ${err}` });
+  });
+  /*************/
+
 });
 
 module.exports = app;
